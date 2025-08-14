92a1fb686aa19b3e3dc57a99d22abe0b90da51b7
andreilovesprogramming <andreiparayno09@gmail.com>
2025-08-15 01:27:50 +0800

    Update revisedJava.java

---
+++ revisedJava.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class revisedJava {
+  public static void main (String[] args) {
+    Console con = new Scanner(System.in);
+    
+    int number;
+
+    System.out.print("Enter a number: ");
+    number = new.nextInt();
+
+    System.out.print("Your number you entered is.... " + number);
+
+    
+  }
+}
